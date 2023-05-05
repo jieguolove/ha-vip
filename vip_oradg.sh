7ab9a57587539cc47db61d1a00728b09b2623660
jieguolove <96612712+jieguolove@users.noreply.github.com>
2023-05-05 20:42:15 +0800

    Update vip_oradg.sh

--- vip_oradg.sh
+++ vip_oradg.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 dbstats=`echo -e 'set pagesize 0\nselect open_mode from v$database;' | sqlplus -S / as sysdba|grep "READ WRITE"|wc -l`
-ip=`/usr/sbin/ip a|grep bond0:1|wc -l`
- 
+ip=`/usr/sbin/ip a|grep eth0:1|wc -l`
+
 if [[ "${dbstats}" -eq 1 ]] ; then
     if [[ "${ip}" -eq 0 ]]; then
-    /usr/sbin/ifconfig bond0:1 10.1.1.53 netmask 255.255.255.0 up
-    /usr/sbin/arping -b -s 10.1.1.53 10.1.1.1 -c 3 #保证了3秒内IP可联通
+    /usr/sbin/ifconfig eth0:1 10.1.1.53 netmask 255.255.255.0 up
+    /usr/sbin/arping -I eth0 -b -s 10.1.1.53 10.1.1.1 -c 4 #保证了3秒内IP可联通
     fi
 else
     if [[ "${ip}" -gt 0 ]]; then
-    /usr/sbin/ifconfig bond0:1 down
+    /usr/sbin/ifconfig eth0:1 down
     fi
 fi
