b405ba5b766f9851f0f4757a6f0faf625fdf9e5d
jieguolove <96612712+jieguolove@users.noreply.github.com>
2023-05-05 20:38:12 +0800

    Update vip_pg.sh

--- vip_pg.sh
+++ vip_pg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-dbstats=`PGPASSWORD="Abcd1234" /opt/pgsql/bin/psql -U postgres -d postgres -p 5432 --host 10.1.1.53 -c "select pg_is_in_recovery();"|grep f|wc -l` ###根据实际情况修改密码和IP信息等
+dbstats=`su - postgres -c "/home/pgsql/bin/repmgr -f /home/pgsql/repmgr.conf cluster show"|grep 本机主机名|grep primary|grep running|wc -l` ###repmgr管理的情况下
+#dbstats=`PGPASSWORD="Abcd1234" /opt/pgsql/bin/psql -U postgres -d postgres -p 5432 --host 10.1.1.53 -c "select pg_is_in_recovery();"|grep f|wc -l` ###非repmgr情况下，根据实际情况修改密码和IP信息等
 ip=`/usr/sbin/ip a|grep bond0:1|wc -l`
  
 if [[ "${dbstats}" -eq 1 ]] ; then
